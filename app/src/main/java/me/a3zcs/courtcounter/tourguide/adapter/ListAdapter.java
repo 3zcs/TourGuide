786058c64b04511a239835fa471d1247a3f490fc
3zcs <abdulaziz.aloshin@gmail.com>
2017-07-21 10:10:29 +0300

    - add image to restaurant list

--- app/src/main/java/me/a3zcs/courtcounter/tourguide/adapter/ListAdapter.java
+++ app/src/main/java/me/a3zcs/courtcounter/tourguide/adapter/ListAdapter.java
@@ -11,9 +11,11 @@ import android.widget.TextView;
 import java.util.List;
 
 import me.a3zcs.courtcounter.tourguide.DetailActivity;
+import me.a3zcs.courtcounter.tourguide.R;
 import me.a3zcs.courtcounter.tourguide.model.Place;
 
 import static me.a3zcs.courtcounter.tourguide.Constant.PLACE;
+import static me.a3zcs.courtcounter.tourguide.Constant.RESTAURANT;
 
 /**
  * Created by root on 19/07/17.
@@ -31,12 +33,25 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.itemViewHolder
     @Override
     public itemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(android.R.layout.simple_list_item_1,parent,false);
-        return new itemViewHolder(view);
+        View view;
+        switch (viewType){
+            case 1:
+                 view = inflater.inflate(R.layout.layout_with_image,parent,false);
+                break;
+            default:
+                 view = inflater.inflate(android.R.layout.simple_list_item_1,parent,false);
+
+        }
+        return new itemViewHolder(view,viewType);
     }
 
     @Override
-    public void onBindViewHolder(itemViewHolder holder, int position) {
+    public int getItemViewType(int position) {
+        return placeList.get(position).getType().equals(RESTAURANT)? 1 : 0;
+    }
+
+    @Override
+    public void onBindViewHolder(itemViewHolder holder, int position ) {
         holder.place.setText(placeList.get(position).getName());
         holder.placeObject = placeList.get(position);
     }
@@ -49,11 +64,16 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.itemViewHolder
     class itemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView place;
         Place placeObject;
-        public itemViewHolder(View view) {
+        public itemViewHolder(View view,int type) {
             super(view);
-            place = view.findViewById(android.R.id.text1);
+            if (type == 0)
+                place = view.findViewById(android.R.id.text1);
+            else
+                place = view.findViewById(R.id.text1);
             view.setOnClickListener(this);
         }
+
+
 
         @Override
         public void onClick(View view) {
